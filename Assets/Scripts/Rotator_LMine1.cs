354b2d9ede20723e77367b571a492caf0d6af854
hk547 <60270709+hk547@users.noreply.github.com>
2020-10-01 12:28:26 -0400

    Setting up mines

---
+++ Assets/Scripts/Rotator_LMine1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator_LMine1 : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -65) * Time.deltaTime);
+    }
+}
