bbeb198194802d93ed8873f82c9a309917e7af62
hk547 <60270709+hk547@users.noreply.github.com>
2020-10-01 04:29:55 -0400

    Player control and stageclear setting

---
+++ Assets/Scripts/RotatorFl1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorFl1 : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 26, 0) * Time.deltaTime);
+    }
+}
+
